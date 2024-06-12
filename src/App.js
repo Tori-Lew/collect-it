89b149583ce2f8ed06da729432c1e0324dfa3ed3
Tori Lew <torirlew@gmail.com>
2024-06-12 16:16:49 -0700

    Tutorial https://www.linkedin.com/learning/react-js-essential-training-14836121/fetching-data-with-graphql?autoSkip=true&resume=false&u=95229113 up to Fetching data with GraphQL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect /*useReducer useRef*/} from "react";
 
-function App() {
+/*function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  return [
+    {value, onChange: e => setValue(e.target.value)},
+    () => setValue(initialValue)
+  ];
+}*/
+function GithubUser({ name, location, avatar }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name}/>
     </div>
-  );
+  )
+}
+
+function App({library}) {
+  //const [emotion, setEmotion] = useState("happy");
+  //const [secondary, setSecondary] = useState("tired");
+  //const [checked, setChecked] = useReducer((checked) => !checked, false);
+  //const txtTitle = useRef();
+  //const hexColor = useRef();
+  //const [title, setTitle] = useState("");
+  //const [color, setColor] = useState("#000000");
+  /*const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
+  const submit = (e) => {
+    e.preventDefault();
+    //const title = txtTitle.current.value;
+    //const color = hexColor.current.value;
+    //alert(`${title}, ${color}`)
+    //txtTitle.current.value = "";
+    //hexColor.current.value = "";
+    //setTitle("");
+    //setColor("#000000");
+    alert(`${titleProps.value}, ${colorProps.value}`)
+    resetTitle();
+    resetColor();
+  }*/
+  /*return (
+    <div className="App">
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
+      <h2>Current secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
+    
+      <input 
+        type="checkbox" 
+        value={checked} 
+        onChange={ setChecked }
+      />
+      <label>
+        {checked ? "checked" : "not checked"}
+      </label>
+    </div>
+    <form onSubmit={submit}>
+      <input 
+        //ref={txtTitle}
+        //value={title}
+        //onChange={event => setTitle(event.target.value)}
+        {...titleProps}
+        type="text"
+        placeholder="color title ..."
+      />
+      <input 
+        //ref={hexColor} 
+        //value={color}
+        //onChange={event => setColor(event.target.value)}
+        {...colorProps}
+        type="color" 
+      />
+      <button>ADD</button>
+    
+    </form>
+
+  );*/
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://api.github.com/users/tori-lew`)
+    .then((response) => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null;
+    return(
+      <GithubUser 
+        name={data.name} 
+        location={data.location} 
+        avatar={data.avatar_url}/>
+    );
 }
 
 export default App;
